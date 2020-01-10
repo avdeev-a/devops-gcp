8cd113c96e59f54b575fcb38beb18c61084617e6
bob <root@avdeev-a.pro>
2020-01-10 14:59:50 +0100

    Change deployment.sh

--- kube/deployment.sh
+++ kube/deployment.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-cd /home/depoyment
-kubectl apply -f namespace.yaml
+cd ../deployment/
+kubectl apply -f Namespace.yaml
 kubectl apply -f PersistentVolume.yaml
 kubectl apply -f PersistentVolumeClaim.yaml
-kubectl apply -f deployment.yaml 
-kubectl apply -f service.yaml
+kubectl apply -f Deployment.yaml 
+kubectl apply -f Service.yaml
 
 echo " ############# Application deployment complete! #############"
